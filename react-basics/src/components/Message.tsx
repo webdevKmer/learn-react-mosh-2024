d3c0cf84900d4c1d478733666041aea4c7dd94e4
webdevKmer <webdevKmer@gmail.com>
2024-05-05 16:11:00 +0000

    add Message component

---
+++ react-basics/src/components/Message.tsx
@@ -0,0 +1,10 @@
+const Message = () => {
+    // const name = "Patrick"
+    const name = ""
+  return (
+    name ? <h2>Hello {name}!</h2> : <h2>Hello World!</h2>
+    
+  )
+}
+
+export default Message
